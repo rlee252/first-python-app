2f42b7644f8458ab502c1e63dbad9913a5736892
Ray <rlee252@gmail.com>
2026-06-26 14:02:30 -0400

    feelings moved to JSON

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
-from feelings_data import feelings
+import json
 
 app = Flask(__name__)
+
+with open("feelings.json", "r") as file:
+    feelings = json.load(file)
+
 
 @app.route("/")
 def home():
     return render_template("index.html", feelings=feelings)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
