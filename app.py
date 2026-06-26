ee9c598025cdd834e9ea19a5c87586b0b000648c
Ray <rlee252@gmail.com>
2026-06-26 13:47:23 -0400

    Create feelings table

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
+from feelings_data import feelings
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", feelings=feelings)
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
---
+++ feelings_data.py
@@ -0,0 +1,62 @@
+feelings = {
+    "Happy": [
+        "Joyful",
+        "Excited",
+        "Proud",
+        "Content",
+        "Grateful",
+        "Optimistic",
+        "Playful",
+        "Loving"
+    ],
+    "Sad": [
+        "Lonely",
+        "Disappointed",
+        "Hurt",
+        "Grief",
+        "Hopeless",
+        "Depressed",
+        "Rejected",
+        "Guilty"
+    ],
+    "Angry": [
+        "Frustrated",
+        "Annoyed",
+        "Irritated",
+        "Resentful",
+        "Jealous",
+        "Bitter",
+        "Furious",
+        "Offended"
+    ],
+    "Fear": [
+        "Anxious",
+        "Nervous",
+        "Overwhelmed",
+        "Worried",
+        "Insecure",
+        "Helpless",
+        "Scared",
+        "Threatened"
+    ],
+    "Surprised": [
+        "Amazed",
+        "Confused",
+        "Curious",
+        "Startled",
+        "Shocked",
+        "Astonished",
+        "Disillusioned",
+        "Speechless"
+    ],
+    "Disgust": [
+        "Repulsed",
+        "Disappointed",
+        "Judgmental",
+        "Embarrassed",
+        "Aversion",
+        "Uncomfortable",
+        "Distaste",
+        "Disapproving"
+    ]
+}
